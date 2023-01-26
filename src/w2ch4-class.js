6d0bce9498cefcc2cc7a2b33f9863f01941dd64b
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-01-26 22:18:20 +0100

    Add js push, some, find

--- src/w2ch4-class.js
+++ src/w2ch4-class.js
@@ -1,1 +1,32 @@
-let test = "test";
+class CodersArray {
+  constructor(...theArgs) {
+    let myArray = {};
+    this.myArray = myArray;
+    for (let i in theArgs) {
+      this.myArray[i] = theArgs[i];
+    }
+  }
+
+  lengtH() {
+    let counter = 0;
+    for (let i in this.myArray) {
+      counter++;
+    }
+    return counter;
+  }
+
+  some(value){
+    let some 
+    for (let i in this.myArray){
+        if (i === value){
+            some = true
+        }
+        else some = false
+    }
+
+    return some
+  }
+
+}
+
+const numbers = new CodersArray(2, 6, 8);
