1a1e4bc66d5d88068f0ce6598be7cac437ad97d0
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-01-27 08:33:42 +0100

    Add maP method

--- src/w2ch4-class.js
+++ src/w2ch4-class.js
@@ -15,33 +15,35 @@ class CodersArray {
     return counter;
   }
 
-  pusH(value){
+  pusH(value) {
     let length = this.lengtH();
-    this.myArray[length]=value;
+    this.myArray[length] = value;
     return this.myArray;
-
   }
 
-  some(value){
-    for (let i of Object.values(this.myArray)){
-        if (i === value){
-            return true
-        }
+  some(value) {
+    for (let i of Object.values(this.myArray)) {
+      if (i === value) {
+        return true;
+      }
     }
 
-    return false
+    return false;
   }
 
-find(value){
+  find(value) {
     let findIt = 0;
     for (let i of Object.values(this.myArray)) {
-      if (value < i)
-      return i;}}
+      if (value < i) return i;
     }
+  }
 
-
-
-
+  maP(expression) {
+    for (i = 0; i < this.lengtH() - 1; i++) {
+      this.myArray[i] = this.myArray[i] * 2;
+    }
+    return this.myArray;
+  }
+}
 
 const numbers = new CodersArray(2, 6, 8);
-
